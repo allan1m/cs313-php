190f22a3fffe953938b138a771f0a6abcbda356a
Allan Marina <allanmarina33@gmail.com>
2021-02-03 14:39:55 -0700

    update file 05

--- web/w05-prove.php
+++ web/w05-prove.php
@@ -9,13 +9,13 @@
     What would you like to do?
     
     <form action="" method="post">
-        <input type="radio" name="employee_name" id="employee_name"><label for="employee_name">List of employees</label> <br>
+        <input type="radio" name="employee_info" id="employee_info"><label for="employee_info">Employee</label> <br>
         <input type="radio" name="employee_schedule" id="empoyee_schedule"><label for="employee_schedule">List of employee schedules</label><br>
         <input type="submit" value="submit" name="Submit">
     </form>
 
     <?php
-        if (isset($_POST['employee_name'])) {
+        if (isset($_POST['employee_info'])) {
             try
             {
                 $dbUrl = getenv('DATABASE_URL');
@@ -47,6 +47,43 @@
                 echo 'Phone Number: ' . $row['phone_number'];
                 echo '<br/>';
             }
+        }
+
+        if (isset($POST['employee_schdule'])) {
+            try
+            {
+                $dbUrl = getenv('DATABASE_URL');
+        
+                $dbOpts = parse_url($dbUrl);
+        
+                $dbHost = $dbOpts["host"];
+                $dbPort = $dbOpts["port"];
+                $dbUser = $dbOpts["user"];
+                $dbPassword = $dbOpts["pass"];
+                $dbName = ltrim($dbOpts["path"],'/');
+        
+                $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $ex)
+            {
+                echo 'Error!: ' . $ex->getMessage();
+                die();
+            }
+
+        
+            foreach ($db->query('SELECT first_name, last_name FROM employee') as $row)
+                foreach ($db->query('SELECT work_date, start_work_time, end_work_time, employee_id FROM schedule') as $row) {
+                    {
+                        echo 'Employee ID: ' . $row['employee_id'];
+                        echo 'Firt Name: ' . $row['first_name'];
+                        echo ' Last Name: ' . $row['last_name'];
+                        echo 'Start Time: ' . $row['phone_number'];
+                        echo 'End Time: ' . $row['phone_number'];
+                        echo '<br/>';
+                    }
+                }
         }
     ?>
 </body>
