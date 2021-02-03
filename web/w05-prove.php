cd1002d129593cd76c5a21cefb3d8a21900559d1
Allan Marina <allanmarina33@gmail.com>
2021-02-03 14:05:16 -0700

    updated file 02

--- web/w05-prove.php
+++ web/w05-prove.php
@@ -9,8 +9,8 @@
     What would you like to do?
     
     <form action="" method="post">
-        <input type="radio" name="employee_name" id="employee_name">
-        <input type="radio" name="employee_schedule" id="empoyee_schedule">
+        <input type="radio" name="employee_name" id="employee_name"><label for="employee_name">List of employees</label> <br>
+        <input type="radio" name="employee_schedule" id="empoyee_schedule"><label for="employee_schedule">List of employee schedules</label><br>
         <input type="submit" value="submit" name="Submit">
     </form>
 </body>
