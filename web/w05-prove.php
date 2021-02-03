36d809ed1fa8262974619d726de6cf6cd8513c94
Allan Marina <allanmarina33@gmail.com>
2021-02-03 14:01:01 -0700

    updating file

---
+++ web/w05-prove.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    What would you like to do?
+    
+    <form action="" method="post">
+        <input type="radio" name="employee_name" id="employee_name">
+        <input type="radio" name="employee_schedule" id="empoyee_schedule">
+        <button type="submit" name="submit" value="Submit"></button>
+    </form>
+</body>
+</html>
