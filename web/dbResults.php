da862be59e4a9bb0396e3e32c33e78c791174de9
Allan Marina <allanmarina33@gmail.com>
2021-02-06 13:28:17 -0700

    update f 02

--- web/dbResults.php
+++ web/dbResults.php
@@ -15,12 +15,10 @@
 
         echo "VAR DUMP: " . var_dump($_SESSION['fname']) . "<br>";
         echo "PRINT R: " . print_r($_SESSION['fname']) . "<br>";
-
         if (isset($_SESSION['fname']) || isset($_SESSION['lname'])) {
             foreach ($_SESSION['fname'] as $key_one => $value_one) {
-                echo $value_one;
                 foreach ($_SESSION['lname'] as $key_one => $value_two) {
-                    echo $value_two . "<br>";
+                    echo $value_one . " " . $value_two;
                 }
             }
         }
