37caa45a65d2ca8afc0e4f6edf0c97bf82787de8
Allan Marina <allanmarina33@gmail.com>
2021-02-10 10:30:23 -0700

    ta 06 update 02

--- web/ta_06.php
+++ web/ta_06.php
@@ -1,28 +1,3 @@
-<?php
-
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,15 +8,44 @@ catch (PDOException $ex)
 </head>
 <body>
 
+<form action="" method="post">
+    <input type="radio" name="script" id="script"><label for="script">Script</label>
+    <input type="submit" value="submit" name="Submit">
+</form>
+
 <?php
 
-    foreach ($db->query('SELECT book, chapter, verse, content password FROM scriptures') as $row)
-    {
-    echo 'Book: ' . $row['book'];
-    echo 'Chapter: ' . $row['chapter'];
-    echo 'Verse: ' . $row['verse'];
-    echo 'Content: ' . $row['content'];
-    echo '<br>';
+    if (isset($_POST['script'])) {
+        try
+        {
+            $dbUrl = getenv('DATABASE_URL');
+
+            $dbOpts = parse_url($dbUrl);
+
+            $dbHost = $dbOpts["host"];
+            $dbPort = $dbOpts["port"];
+            $dbUser = $dbOpts["user"];
+            $dbPassword = $dbOpts["pass"];
+            $dbName = ltrim($dbOpts["path"],'/');
+
+            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+            echo 'Error!: ' . $ex->getMessage();
+            die();
+        }
+        
+        foreach ($db->query('SELECT book, chapter, verse, content password FROM scriptures') as $row)
+        {
+            echo 'Book: ' . $row['book'];
+            echo 'Chapter: ' . $row['chapter'];
+            echo 'Verse: ' . $row['verse'];
+            echo 'Content: ' . $row['content'];
+            echo '<br>';
+        }
 }
 
 ?>
