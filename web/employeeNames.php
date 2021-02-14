0e919e160184f3b83e1f22a94405c3a18122925a
Allan Marina <allanmarina33@gmail.com>
2021-02-13 20:59:20 -0700

    updated file

--- web/employeeNames.php
+++ web/employeeNames.php
@@ -8,19 +8,25 @@ $db = get_db();
 
 if (empty($_SESSION['fname']) && isset($_POST['employee_n'])) {    
 
-    foreach ($db->query('SELECT first_name, last_name, id FROM employee') as $row)
+    foreach ($db->query('SELECT first_name, last_name, id, address, phone_number FROM employee') as $row)
     {
         $_SESSION['fname'][] = $row['first_name'];
         $_SESSION['lname'][] = $row['last_name'];
         $_SESSION['id'][] = $row['id'];
+        $_SESSION['address'][] = $row['address'];
+        $_SESSION['phone_number'][] = $row['phone_number'];
     }
 }
 
 if (empty($_SESSION['date']) && isset($_POST['schedule'])) {
                    
-    foreach ($db->query('SELECT work_date FROM schedule') as $row) {
+    foreach ($db->query('SELECT id, work_date, start_work_time, end_work_time, employee_id FROM schedule') as $row) {
             {
-                $_SESSION['date'][] = $row['work_date'];
+                $_SESSION['id'][] = $row['id'];
+                $_SESSION['work_date'][] = $row['work_date'];
+                $_SESSION['start_work_time'][] = $row['start_work_time'];
+                $_SESSION['end_work_time'][] = $row['end_work_time'];
+                $_SESSION['employee_id'][] = $row['employee_id'];
             }
         }
 }
