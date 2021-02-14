ded68a8ae50e0e62c6fdc40673427c0e441ad100
Allan Marina <allanmarina33@gmail.com>
2021-02-13 19:32:03 -0700

    updated file

--- web/employeeNames.php
+++ web/employeeNames.php
@@ -8,9 +8,8 @@ $db = get_db();
 
 if (empty($_SESSION['fname']) && isset($_POST['employee_n'])) {    
 
-    foreach ($db->query('SELECT id, first_name, last_name, FROM employee') as $row)
+    foreach ($db->query('SELECT first_name, last_name FROM employee') as $row)
     {
-        //$_SESSION['id'][] = $row['id'];
         $_SESSION['fname'][] = $row['first_name'];
         $_SESSION['lname'][] = $row['last_name'];
     }
