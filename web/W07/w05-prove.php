fc6116b8a1e91ddf04b71cdf3c1d7533cbe0883c
Allan Marina <allanmarina33@gmail.com>
2021-02-16 14:12:59 -0700

    team activity files

--- web/W07/w05-prove.php
+++ web/W07/w05-prove.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="dbresults.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -12,14 +13,15 @@
     What would you like to do?
 
     <a href="dbResults.php">Go To Results</a>
-    
-    <form action="" method="post">
-        <input type="checkbox" name="employee_name" id="employee_n"><label for="employee_n">Employee Names</label> <br>
-        <input type="checkbox" name="employee_address" id="employee_address"><label for="employee_address">Employee Address</label> <br>
-        <input type="checkbox" name="employee_phone_number" id="employee_phone_number"><label for="employee_phone_number">Employee Phone Number</label> <br>
-        <input type="checkbox" name="work_date" id="work_date"><label for="work_date">Employee schedule</label><br>
-        <input type="checkbox" name="employee_shift" id="employee_shift"><label for="employee_shift">Employee Shift</label> <br>
-        <input type="submit" value="submit" name="Submit">
-    </form>
+    <div class="container01">
+        <form action="" method="post">
+            <input type="checkbox" name="employee_name" id="employee_n"><label for="employee_n">Employee Names</label> <br>
+            <input type="checkbox" name="employee_address" id="employee_address"><label for="employee_address">Employee Address</label> <br>
+            <input type="checkbox" name="employee_phone_number" id="employee_phone_number"><label for="employee_phone_number">Employee Phone Number</label> <br>
+            <input type="checkbox" name="work_date" id="work_date"><label for="work_date">Employee schedule</label><br>
+            <input type="checkbox" name="employee_shift" id="employee_shift"><label for="employee_shift">Employee Shift</label> <br>
+            <input type="submit" value="submit" name="Submit">
+        </form>
+    </div>
 </body>
 </html>
