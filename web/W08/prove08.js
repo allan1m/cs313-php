edec75edd67fe4efc5247f866bbb501e89827335
Allan Marina <allanmarina33@gmail.com>
2021-02-25 11:18:37 -0700

    Updated prove08.js

--- web/W08/prove08.js
+++ web/W08/prove08.js
@@ -1,40 +1,36 @@
-var http = require('http'); //1 - Import Node.js core Module
-var url = require('url');
+const http = require('http'); //1 - Import Node.js core Module
+const port = 8080;
 
-var server = http.createServer(function(req, res){ //2 - Create server
+function home(res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<h1>Welcome to the Home Page</h1>");
+    res.end();
+}
+
+function getData(res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const info = { name: "Allan Marina", class: "CS313" };
+    res.write(JSON.stringify(info));
+    res.end();
+}
+
+const requestListener = (req, res) => { 
 
     if (req.url == '/home') {
-        
         home(res);
     }
-    else if(req.url == '/getData'){
+    if(req.url == '/getData'){
         getData(res);
     }
-    else{
-        renderPage(res);
-    }
-
-});
-
-function home(res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
-    res.write('<h1> Welcome to the Home Page</h1>');
+    
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write("<h1>Page not found</h1>");
     res.end();
-}
+    
+    
 
-function getData(res){
-    res.writeHead(200, {'Content-Type': 'application/json'});
+};
 
-    var obj = {"name": "Allan", "class":"CSE341"};
-    res.write(JSON.stringify(obj));
-    res.end();
-}
+var server = http.createServer(requestListener);
 
-function renderPage(res){
-    res.writeHead(404, {'Content-Type': 'tex/plain'});
-    res.write('Page Not Found');
-    res.end();
-}
-
-server.listen(8888, '70.171.254.178');
+server.listen(port);
